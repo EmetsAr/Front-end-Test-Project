dbb46d84509a5a3af0c266959bc1d20b58d576e9
EmetsAR <tema_patlatui@mail.ru>
2015-11-01 15:27:45 +0100

    test task styling

---
+++ dist/js/main.js
@@ -0,0 +1,43 @@
+/* ==========================================================================
+
+    Project: Test Project
+    Author: XHTMLized
+    Last updated: Sun Nov 01 2015 13:41:10
+
+   ========================================================================== */
+
+(function($) {
+
+  'use strict';
+
+  var App = {
+
+    /**
+     * Init Function
+     */
+    init: function() {
+      // App.feature1();
+      // App.feature2();
+    },
+
+    /**
+     * Custom feature 1
+     */
+    feature1: function() {
+
+    },
+
+    /**
+     * Custom feature 2
+     */
+    feature2: function() {
+
+    }
+
+  };
+
+  $(function() {
+    App.init();
+  });
+
+})(jQuery);
