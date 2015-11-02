5d3cd21a850220d8635c82dff9568c8fdfa00057
EmetsAR <emetsar88@gmail.com>
2015-11-02 13:02:08 +0100

    js fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@
 
     Project: Test Project
     Author: XHTMLized
-    Last updated: @@timestamp
+    Last updated: Mon Nov 02 2015 12:52:28
 
    ========================================================================== */
 
@@ -11,60 +11,32 @@
 
   'use strict';
 
-$('a.js_gallery').colorbox({
-  rel:'js_gallery',
-  loop:false,
-  width:'500',
-  slideshow:true,
-  slideshowAuto:false,
-  onComplete:function(){
-  }
-});
+  $('a.js_gallery').colorbox({
+      rel: 'js_gallery',
+      loop: false,
+      width: '500',
+      slideshow: true,
+      slideshowAuto: false,
+      onComplete: function() {}
+    })
+    .first().trigger("click");
 
-$(window).load(function(){
-  $('a.js_gallery').first().trigger("click");
 
-  $(document).bind("cbox_complete", function(){
+
+
+  $(document).bind("cbox_complete", function() {
+
     setTimeout($.colorbox.next, 2000);
-    var opener=$('a.js_gallery'),
-        index= $.colorbox.element().index('a.js_gallery')
-    if(index==(opener.length-1)){
+
+    var opener = $('a.js_gallery'),
+      index = $.colorbox.element().index('a.js_gallery');
+
+    if (index == (opener.length - 1)) {
       setTimeout(function() {
-             $.colorbox.close()
+        $.colorbox.close()
       }, 2000);
-     }
-  });
-})
-
-
-  var App = {
-
-    /**
-     * Init Function
-     */
-    init: function() {
-      // App.feature1();
-      // App.feature2();
-    },
-
-    /**
-     * Custom feature 1
-     */
-    feature1: function() {
-
-    },
-
-    /**
-     * Custom feature 2
-     */
-    feature2: function() {
-
     }
-
-  };
-
-  $(function() {
-    App.init();
   });
+
 
 })(jQuery);
--- src/js/main.js
+++ src/js/main.js
@@ -19,52 +19,25 @@ $('a.js_gallery').colorbox({
   slideshowAuto:false,
   onComplete:function(){
   }
-});
+})
+.first().trigger("click");
 
-$(window).load(function(){
-  $('a.js_gallery').first().trigger("click");
+
+
 
   $(document).bind("cbox_complete", function(){
+
     setTimeout($.colorbox.next, 2000);
+
     var opener=$('a.js_gallery'),
-        index= $.colorbox.element().index('a.js_gallery')
+        index= $.colorbox.element().index('a.js_gallery');
+
     if(index==(opener.length-1)){
       setTimeout(function() {
              $.colorbox.close()
       }, 2000);
      }
   });
-})
 
-
-  var App = {
-
-    /**
-     * Init Function
-     */
-    init: function() {
-      // App.feature1();
-      // App.feature2();
-    },
-
-    /**
-     * Custom feature 1
-     */
-    feature1: function() {
-
-    },
-
-    /**
-     * Custom feature 2
-     */
-    feature2: function() {
-
-    }
-
-  };
-
-  $(function() {
-    App.init();
-  });
 
 })(jQuery);
