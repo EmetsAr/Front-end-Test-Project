9c8d4d04a5ec912d22a6d9d23ee4a257063162b7
EmetsAR <emetsar88@gmail.com>
2015-11-03 13:21:32 +0100

    QA review#1 fix

--- src/js/main.js
+++ src/js/main.js
@@ -10,34 +10,33 @@
 
 
   'use strict';
+  var autoNav;
 
-$('a.js_gallery').colorbox({
-  rel:'js_gallery',
-  loop:false,
-  width:'500',
-  slideshow:true,
-  slideshowAuto:false,
-  onComplete:function(){
-  }
-})
-.first().trigger("click");
+  $('a.js_gallery').colorbox({
+      rel: "js_gallery",
+      loop: false,
+      width: "400",
+      slideshow: true,
+      slideshowAuto: false,
+      onComplete: function() {
+        autoNav = setTimeout($.colorbox.next, 2000);
+      },
+      onCleanup: function() {
+        clearTimeout(autoNav);
+      }
+    })
+    .first().trigger("click");
 
+  $(document).bind("cbox_complete", function() {
 
+    var opener = $('a.js_gallery'),
+        index = $.colorbox.element().index('a.js_gallery');
 
-
-  $(document).bind("cbox_complete", function(){
-
-    setTimeout($.colorbox.next, 2000);
-
-    var opener=$('a.js_gallery'),
-        index= $.colorbox.element().index('a.js_gallery');
-
-    if(index==(opener.length-1)){
+    if (index == (opener.length - 1)) {
       setTimeout(function() {
-             $.colorbox.close()
+        $.colorbox.close();
       }, 2000);
-     }
+    }
   });
-
 
 })(jQuery);
